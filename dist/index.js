c7ae864cb9ca3a864e2c8de3d30e3ebfd39371a2
Addisu Z. Taddese <addisu@openrobotics.org>
2024-12-06 14:13:05 -0600

    Try more ways to debug
    Signed-off-by: Addisu Z. Taddese <addisu@openrobotics.org>

--- dist/index.js
+++ dist/index.js
@@ -36019,7 +36019,9 @@ async function run(_local, _lib, _branch) {
 
     if (!_local && labels.length > 0) {
       const prNumber = github.context.payload.pull_request.number;
+      core.info(`::debug:: Adding labels: [${labels}] to PR [${prNumber}]`);
       core.info(`Adding labels: [${labels}] to PR [${prNumber}]`);
+      core.notice(`Adding labels: [${labels}] to PR [${prNumber}]`);
       gh.rest.issues.addLabels(
         Object.assign({issue_number: prNumber, labels: labels },
         github.context.repo));
